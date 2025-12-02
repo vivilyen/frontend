47ac9a6752113dd5b4d52f391a915ab13de82735
Vivien Gőgh <goghvivien2011@gmail.com>
2025-12-02 19:02:24 +0100

    upload

---
+++ react/lesson 1/lesson_1.js
@@ -0,0 +1,7 @@
+const socksPrice = 10;
+const tshirtPrice = 8;
+const shippingCost = 5;
+let productCost = socksPrice + 2 * tshirtPrice;
+const todayDate = dayjs().format('MMMM D');
+let currentTime = dayjs().format('HH:mm:ss');
+
